2582a5b2774232e950d4973a171be420ea14d360
dhjeon <jeondh9971@gmail.com>
2022-05-05 12:15:26 +0900

    액션바 삭제

---
+++ app/src/main/java/com/wecan/letsgo/Activity_Launch.java
@@ -0,0 +1,22 @@
+package com.wecan.letsgo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Activity_Launch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(Activity_Launch.this, Activity_Main.class);
+            startActivity(intent);
+        },2000);
+    }
+}
--- app/src/main/java/com/wecan/letsgo/Activity_Main.java
+++ app/src/main/java/com/wecan/letsgo/Activity_Main.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ActivityLaunch extends AppCompatActivity {
+public class Activity_Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launch);
+        setContentView(R.layout.activity_main);
     }
 }
